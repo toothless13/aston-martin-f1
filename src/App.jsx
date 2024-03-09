637fb86bb55462a81b0a1cdc1242b18c3c752514
Tim <50870607+toothless13@users.noreply.github.com>
2024-03-09 13:48:44 +0000

    initial commit and boilerplate setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className="px-20">
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
