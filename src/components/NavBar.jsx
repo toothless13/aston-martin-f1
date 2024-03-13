d704925261711d31cc9bfc965503bba78624502d
Tim <50870607+toothless13@users.noreply.github.com>
2024-03-13 18:53:54 +0000

    move logo to assets folder

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
   return (
     <div className=" h-20 border-b-2 border-amlime">
       {/* <h1 className="text-4xl w-1/2 text-center py-4 mx-auto">Aston Martin F1</h1> */}
-      <img src="public/logo.png" className="w-1/12 mx-auto"></img>
+      <img src="/assets/logo.png" className="w-1/12 mx-auto"></img>
     </div>
   )
 }
